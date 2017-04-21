39f4a6f405f1afba9f07d74c215ee20f56f0d150
Dao Khanh Vu LE <ledkvu@ymail.com>
2017-04-21 04:09:51 +0200

    qq fixes et nettoyage des fonctions, possible de tester la conversion de coordonées

--- VirtualWhiteboard/VirtualWhiteboard/Interface.h
+++ VirtualWhiteboard/VirtualWhiteboard/Interface.h
@@ -7,8 +7,17 @@
 #define black CV_RGB(0,0,0)
 #define bound CV_RGB(127,127,127)
 
+struct CalibrationData
+{
+	int WS = 1600, HS = 900;
+	int W = 1, H = 1;
+	Point centerDecalage = Point(0, 0);
+	Point2i p1 = Point(0, 0), p2 = Point(0, 0);
+};
 
 double dist(Point x, Point y);
 pair<Point, double> circleFromPoints(Point p1, Point p2, Point p3);
-Mat drawUI(int WS, int HS, Scalar color, int thick, vector<int> &xBounds, vector<int> &yBounds);
+Point convertCoord(Point p, CalibrationData calibr);
+void createBoundsUI(int WS, int HS, vector<int> &xBounds, vector<int> &yBounds);
+Mat drawUI(int WS, int HS, Scalar color, int thick);
 int DetectionUI();
